f80bbad25e7fdca0261c70fffdb96b89dd99d016
cbrgm <chris@cbrgm.de>
2018-01-21 23:02:50 +0000

    Added dependency download

--- tools/install-ansible2-2-0.sh
+++ tools/install-ansible2-2-0.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 wget -O ansible.deb 'http://arnesonium-downloads.s3.amazonaws.com/ansible_2.2.0.0-100.git201611010320.cdec853.HEAD~unstable_all.deb'
 sudo dpkg -i ansible.deb
+sudo apt-get install -f
 rm -f ansible.deb
